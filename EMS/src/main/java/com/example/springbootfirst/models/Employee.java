59468b9aaae7e2b6eb70a9dbed6ab31878c2c544
MukishKumar-T <mukishkumar.t2023cse@sece.ac.in>
2025-07-15 14:06:47 +0530

    EMS Backend

---
+++ EMS/src/main/java/com/example/springbootfirst/models/Employee.java
@@ -0,0 +1,44 @@
+package com.example.springbootfirst.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Employee {
+    @Id
+    private int empID;
+    private String name;
+    private String job;
+
+    public int getEmpId() {
+        return empID;
+    }
+
+    public void setEmpId(int empID) {
+        this.empID = empID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+}
